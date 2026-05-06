af5b5212a01e74cbe1ac855ea21c13dcf0ba7dda
Abner <villasboasdejp@gmail.com>
2026-05-06 10:07:07 -0300

    temperatura

---
+++ tema_.c/tema_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float temperatura = 10.0;
+
+    if (temperatura > 30.0){
+        printf("Está calor\n");
+
+    }
+        else if (temperatura < 15.0){
+            printf("Está muito frio\n");
+        }
+        else {
+            printf("Está agradável\n");
+        }
+}
